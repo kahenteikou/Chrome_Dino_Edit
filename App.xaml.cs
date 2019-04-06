19c3045901886969aeb1300e1aca29841cad348c
kokkiemouse <39451248+kokkiemouse@users.noreply.github.com>
2019-04-06 16:53:50 +0900

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Chromedino1
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chromedino1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Microsoft.VisualBasic.Devices.Audio audio =
+    new Microsoft.VisualBasic.Devices.Audio();//BGM再生用
+        public MainWindow()
+        {
+            InitializeComponent();
+            var browser = new CefSharp.Wpf.ChromiumWebBrowser();
+            browser.Address = "file://" + AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "\\DINODATA\\Index.html";
+            Content = browser;
+            audio.Play(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "\\DINODATA\\Assets\\BGM.wav", Microsoft.VisualBasic.AudioPlayMode.BackgroundLoop);
+
+
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            audio.Stop();
+            CefSharp.Cef.Shutdown();
+        }
+    }
+}
